05e756ac9089307cb4b10fa31a986888d454b83c
Hamza Benarfa <hamzabenarfa4@gmail.com>
2024-09-25 17:31:51 +0100

    Add express dependency and implement POST endpoint

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello wordddgsrg")
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+
+    console.log(typeof parseInt(id));
+
+    const data = req.body;
+    
+    res.status(201).json({ name: data.name, age: data.age, id });
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+const PORT = 4000;
+app.listen(PORT);
